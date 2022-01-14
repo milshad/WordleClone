f7add10e2b09def9fcd550182c300c9c4fc41205
shadpourmillad@gmail.com <shadpourmillad@gmail.com>
2022-01-14 21:51:05 +0000

    added api functions & what right answer is on failed attempts

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # WordleClone
-Clone of popular game worlde in order to find best strategy
+Clone of popular game Wordle
 
-Going to emulate a basic version of worlde game
+Being made in order to find out best strategy as well as cracking the best possible route
 
-Going to play game until best strategy found 
+Also because I have no sense of purpose and moving from one project to another gives me purpose until my next mental brekdown
 
-Going to create algorithm to break game
+HOW TO PLAY:
 
+CLONE THE REPO OR TAKE THE CODE IN GAME.PY AND RUN IT IN ANY PYTHON IDE (VSCODE ETC)
+IF WANTING TO PLAY IN BROWSER TAKE CODE AND MAKE REPL.IT ACCOUNT AND YOU CAN PASTE THERE ALL PACKAGES ARE SUPPORTED THERE
 
-version 1:
+VERSION 1:
 
 Added basic functions, game works properly no API yet & only playable with fixed word currently
+
+VERSION 2:
+
+Added API function, game now playable with random words.
+Game now also tells you what the right answer is as opposed to before (nested loop fixed)
+
+To Be Added:
+
+When word is guessed, check against dictionary to see if a real world like wordle.
+Basic GUI. 
--- game.py
+++ game.py
@@ -1,9 +1,29 @@
+import requests
+import json
+import random 
+from array import array
+
+# makes a request for 1000 words from a dictionary api
+resp = requests.get("https://random-word-api.herokuapp.com/word?number=1000")
+
+# parses the response to json so we can loop through easy
+json_response = json.loads(resp.text)
+
+# array to enter the five letter words in
+five_letter_words = []
+
+# loop going through response looking for 5 length words and adding
+for response in json_response:
+  if len(response) == 5:
+    five_letter_words.append(response)
+
+# finding length of array, choosing a random number between the range & choosing a random word to be the word
+
+upper_range = len(five_letter_words)
+random_word_index = random.randint(0, upper_range)
+random_word = five_letter_words[random_word_index].upper()
 
 
-# initialise the word we want to play with
-
-
-word = ["H", "O", "U", "S", "E"]
 
 # user has six attempts before game ends
 attempts = 6
@@ -32,7 +52,7 @@ available_alphabet = [
 ]
 
 # will need to be a function so we can use recursion feeding back necessary variables requried to continue
-def game_sorter(count, attempts, available_alphabet, correct_answer, word): 
+def game_sorter(count, attempts, available_alphabet, correct_answer, random_word): 
 
 
   # letters in a guess where they are not in the word at all and will be added to unnavailable_alphabet
@@ -53,31 +73,35 @@ def game_sorter(count, attempts, available_alphabet, correct_answer, word):
 
   guess_array = list(guess) # takes the guess and splits it into array to match word format
   
+  if (attempts == 0):
+    print(f"You ran out of attempts the word was: {random_word}")
+  
   while ( (attempts != 0) and (correct_answer != True) ):             
-    if (guess_array == word):                               
+    if (guess_array == random_word):                               
       print("Congratulations you have got the word!")         
       correct_answer = True
       break
     for guess_letter in guess_array: # for each letter in array we do some checks
-      if not (guess_letter in word): # if the letter is not in word we add to the two arrays then continue to next letter
+      if not (guess_letter in random_word): # if the letter is not in word we add to the two arrays then continue to next letter
         available_alphabet.remove(guess_letter) # removes a letter from the main alphabet if not in the main word
         count += 1
         continue
-      if (guess_letter == word[count]): # if the letter matches the letter in same position in main word  it adds it to good letters
+      if (guess_letter == random_word[count]): # if the letter matches the letter in same position in main word  it adds it to good letters
         guess_good_letters.append(guess_letter) # array so you know where the letter belongs
         count += 1
         continue
       else: 
         guess_wrong_position_letters.append(guess_letter) # if the letter is in the word and not in right place then falls into this array
         count += 1
-    else:
-      print(f"You ran out of attempts the word was: {word}")
 
+    # telling user his results of his guess
     print(f"These letters were in the right spot: {guess_good_letters}")
     print(f"These letters were correct but in the wrong spot: {guess_wrong_position_letters}")
     print(f"These letters are the current available letters: {available_alphabet}")
+    
+    # reducing attempts & re-running function recursively with new values
     attempts -= 1
-    game_sorter(0, attempts, available_alphabet, correct_answer, word)
+    game_sorter(0, attempts, available_alphabet, correct_answer, random_word)
 
   
-game_sorter(0, attempts, available_alphabet, correct_answer, word)
+game_sorter(0, attempts, available_alphabet, correct_answer, random_word)
